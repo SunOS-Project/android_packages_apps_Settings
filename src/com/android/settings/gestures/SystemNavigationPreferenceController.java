8b2ec924828c2d3314717f57eb6df02a815fcd66
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-03 10:18:08 -0700

    Settings: Only show gesture navigation if device has a software nav bar
    Change-Id: Ic90c8a194a736807c2e101ffd5f7e90c86a55381

--- src/com/android/settings/gestures/SystemNavigationPreferenceController.java
+++ src/com/android/settings/gestures/SystemNavigationPreferenceController.java
@@ -23,9 +23,15 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.RemoteException;
+import android.view.Display;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
+
+import lineageos.providers.LineageSettings;
 
 public class SystemNavigationPreferenceController extends BasePreferenceController {
 
@@ -54,9 +60,22 @@ public class SystemNavigationPreferenceController extends BasePreferenceControll
 
     /** Returns {@code true} if gesture is available. */
     public static boolean isGestureAvailable(Context context) {
+        boolean hasNavigationBar = false;
+        final boolean configEnabled = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_swipe_up_gesture_setting_available);
+
+        try {
+            IWindowManager windowManager = WindowManagerGlobal.getWindowManagerService();
+            hasNavigationBar = LineageSettings.System.getInt(
+                    context.getContentResolver(),
+                    LineageSettings.System.FORCE_SHOW_NAVBAR,
+                    windowManager.hasNavigationBar(Display.DEFAULT_DISPLAY) ? 1 : 0) == 1;
+        } catch (RemoteException ex) {
+            // no window manager? good luck with that
+        }
         // Skip if the swipe up settings are not available
-        if (!context.getResources().getBoolean(
-                com.android.internal.R.bool.config_swipe_up_gesture_setting_available)) {
+        // or if on-screen navbar is disabled (for devices with hardware keys)
+        if (!configEnabled || !hasNavigationBar) {
             return false;
         }
 
